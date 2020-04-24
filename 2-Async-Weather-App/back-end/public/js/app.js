6907054bb0f352d61f58c3901043bf78ef675054
Roger Takeshita <roger.takeshita@gmail.com>
2020-04-24 17:04:17 -0400

    Finished Weather App using Node and serving hbs to the user
    * public/js/app.js
    
        - Created a JS client side to fetch for the weather using fetch
        - Added eventListener to the button
        - Used some querySelector to target some #ids
    
    * src/routes/weather.js
    
        - Created the /weather router, to fetch for geocode and forecast APIs
    
    * src/utils/forecast.js
    * src/utils/geocode.js
    
        - Same utlis that we had before, now in the back-end

--- 2-Async-Weather-App/back-end/public/js/app.js
+++ 2-Async-Weather-App/back-end/public/js/app.js
@@ -1,1 +1,23 @@
 console.log('Client side is loading');
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    messageOne.textContent = 'Loading...';
+    messageTwo.textContent = '';
+    fetch(`http://localhost:3000/weather?address=${search.value}`).then((response) => {
+        response.json().then(({ error, location, forecast }) => {
+            if (error) {
+                messageOne.textContent = error;
+                messageTwo.textContent = '';
+            } else {
+                messageOne.textContent = location;
+                messageTwo.textContent = `The temperature now is ${forecast.temperature} and feels like ${forecast.feelslike}`;
+            }
+        });
+    });
+});
