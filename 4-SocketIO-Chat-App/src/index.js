bf0bd226dc2af45a7097d3e537c931ed01892dca
Roger Takeshita <roger.takeshita@gmail.com>
2020-05-01 19:24:43 -0400

    Add Mustache, Moment, Qs libraries, add more features to Socket.io
        - mustache - to render the messages on the browser
            <script src="https://cdnjs.cloudflare.com/ajax/libs/mustache.js/3.0.1/mustache.min.js"></script>
        - moment -  to manipulate time, to show the time that we send the message
            <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.22.2/moment.min.js"></script>
        - qs - to setup username/names in the session
            <script src="https://cdnjs.cloudflare.com/ajax/libs/qs/6.6.0/qs.min.js"></script>
    
        - Created broadcasting events
        - Events Acknowledgements
        - Timestamps
        - Socket.io Rooms
        - Storing users ({id, username, room})
        - Auto scrolling

--- 4-SocketIO-Chat-App/src/index.js
+++ 4-SocketIO-Chat-App/src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
-const hbs = require('hbs');
 const http = require('http');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
+const { generateMessage, generateLocationMessage } = require('./utils/messages');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./utils/user');
 const port = process.env.PORT;
 
 const app = express();
@@ -10,22 +12,60 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 const publicDirPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, './templates/views');
-const partialsPath = path.join(__dirname, './templates/partials');
 
-app.set('view engine', 'hbs');
-app.set('views', viewsPath);
-hbs.registerPartials(partialsPath);
 app.use(express.static(publicDirPath));
 
-let count = 0;
 //! Listening for users to connect
 io.on('connection', (socket) => {
     console.log('New Wesocket Connection');
-    socket.emit('countUpdate', count);
-    socket.on('increment', () => {
-        count++;
-        io.emit('countUpdate', count);
+
+    //+ acknowledgements, callback to send status
+    socket.on('join', ({ username, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, username, room });
+
+        if (error) return callback(error);
+        socket.join(user.room);
+        socket.emit('message', generateMessage(user.username, 'Welcome'));
+        socket.broadcast
+            .to(user.room)
+            .emit('message', generateMessage(user.username, `${user.username} has joined`));
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        });
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+        const filter = new Filter();
+        if (filter.isProfane(message)) return callback('Profanity is not allowed');
+        io.to(user.room).emit('message', generateMessage(user.username, message));
+        callback();
+    });
+
+    socket.on('sendLocation', (coords, callback) => {
+        const user = getUser(socket.id);
+        io.to(user.room).emit(
+            'locationMessage',
+            generateLocationMessage(
+                user.username,
+                `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+            )
+        );
+        callback();
+    });
+
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('message', generateMessage(user.username, `${user.username} has left`));
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            });
+        }
     });
 });
 
